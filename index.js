17e6964a3e80d7914e73d6992cb553e09b8b378d
ALAN JOSE <alanjose04231120@gmail.com>
2022-11-17 20:30:24 -0500

    created prompt and then

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 const mark = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [{
+inquirer
+  .prompt([{
     type: 'input',
     name: 'title',
     message: 'what is the title?'
@@ -44,13 +45,16 @@ const questions = [{
     name: 'contribute',
     message: 'Any notes on contribution to repo? '
 }
-];
+]).then();
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+
+}
 
 // Function call to initialize app
 init();
